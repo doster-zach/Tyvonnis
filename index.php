2379231cac86a59b5ce677e00fcc38fdac7aefa8
doster-zach <48426594+doster-zach@users.noreply.github.com>
2019-04-24 19:46:31 -0500

    Initial Commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Tyvannis</title>
+        <?php require './partials/head.php'; ?>
+    </head>
+    <body>
+        <?php require './partials/nav.php'; ?>
+        <div class="container-full">
+            <div class="hero-container">
+
+            </div>
+        </div>
+        <div class="container">
+        </div>
+    </body>
+</html>
---
+++ partials/nav.php
@@ -0,0 +1,15 @@
+<nav>
+    <div class="nav-left">
+        <ul>
+            <li class="nav-logo"><img src="../assets/general/logo.webp" alt="Tyvonni's"</li>
+        </ul>
+    </div>
+    <div class="nav-right">
+        <ul>
+            <li>Menu</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </div>
+</nav>
+<placeholder class="nav-placeholder"></placeholder>

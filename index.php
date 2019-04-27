cc33800a2069a2e9760e18699b31793f9f6e0ca2
Zachary Doster <zacharydoster@Zacharys-MacBook-Pro.local>
2019-04-27 18:30:19 -0500

    Home page sections

--- index.php
+++ index.php
@@ -8,10 +8,95 @@
         <?php require './partials/nav.php'; ?>
         <div class="container-full">
             <div class="hero-container">
-
+                <div class="img-overlay">
+                </div>
+                <div class="hero-text-content">
+                    <h1>Local Catering</h1>
+                    <h2>Custom meals made for you</h2>    
+                </div>
+                <div class="hero-button">
+                    <button>
+                        Contact Us
+                    </button>
+                </div>
             </div>
         </div>
         <div class="container">
+            <!-- Complete your order -->
+            <h1 class="image-breakdown-header">Complete Your Order</h1>
+            <div class="image-breakdown-container">
+                <div class="breakdown-full">
+                    <div class="img-overlay">
+                    </div>
+                    <div class="breakdown-text">
+                        <h2>Put together your own meals</h2>    
+                    </div>
+                    <div class="breakdown-button">
+                        <button>
+                            Most Popular
+                        </button>
+                    </div>
+                </div>
+                <div class="breakdown-wrapper">
+                    <div class="breakdown-one-half left">
+                        <div class="img-overlay">
+                        </div>
+                        <div class="breakdown-text">
+                            <h2>About Us</h2>    
+                        </div>
+                        <div class="breakdown-button">
+                            <button>
+                                Our Story
+                            </button>
+                        </div>
+                    </div>
+                    <div class="breakdown-one-half right">
+                        <div class="img-overlay">
+                        </div>
+                        <div class="breakdown-text">
+                            <h2>Custom Orders</h2>    
+                        </div>
+                        <div class="breakdown-button">
+                            <button>
+                                Contact Us
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div><!-- End of complete your order -->
+            <!-- Steps -->
+            <div class="steps-section">
+                <h1>Ordering With Us</h2>
+                <div class="steps-left">
+                    <div class="step-image pad-bottom">
+                        <img src="/assets/home/step-1.jpg" alt="spices"/>
+                    </div>
+                    <div class="step">
+                        <h2>Next</h2>
+                        <p>In sed orci a lorem placerat sodales id vel urna. Praesent vel sodales nunc. Sed in lorem vitae dolor cursus fringilla. Vivamus tortor justo, bibendum vitae lacus ut, bibendum cursus leo. In quis metus mauris. Aliquam nec ligula vitae neque sodales bibendum quis nec nisi. Pellentesque eleifend, mi eget elementum mattis, ipsum erat convallis mi, quis malesuada nisi sem eget dolor. Sed fermentum in orci ut interdum. Nulla facilisi. Maecenas id scelerisque dolor, non sollicitudin ex. Phasellus dictum pharetra urna, nec convallis sapien consectetur quis. Quisque pharetra, odio auctor pellentesque vulputate, nunc justo imperdiet mi, ut rhoncus enim sapien vel magna. Phasellus lobortis neque at fringilla sollicitudin.
+                        Suspendisse tempor tempus orci eget ullamcorper. Etiam mauris nibh, auctor a mauris sit amet, convallis ornare velit. Aenean vulputate, sapien ac auctor sollicitudin, diam sapien venenatis diam, sed elementum nibh diam sed tortor. Phasellus iaculis nisl quis tellus ullamcorper vehicula. Vestibulum congue eget leo vel eleifend. Nullam vulputate leo nec sodales tincidunt. Etiam odio ligula, faucibus pharetra nulla a, euismod pretium massa. Quisque iaculis a elit ac efficitur.</p>
+                    </div>
+                    <div class="step-image pad-top">
+                        <img src="/assets/home/step-4.jpg" alt="spices"/>
+                    </div>
+                </div>
+                <div class="steps-right">
+                    <div class="step">
+                        <h2>First</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum et dignissim nunc. Morbi erat neque, dignissim sed rhoncus vel, aliquam vel lacus. In tellus ex, ornare at urna ut, rhoncus dapibus lacus. Curabitur vel volutpat nibh. Vivamus et ullamcorper augue. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Fusce in purus velit. Vestibulum non sagittis purus, ut laoreet arcu. Nunc congue turpis dui, quis fermentum tellus efficitur ac.</p>
+                    </div>
+                    <div class="step-image pad-bottom">
+                        <img src="/assets/home/step-2.jpg" alt="spices"/>
+                    </div>
+                    <div class="step-image pad-top">
+                        <img src="/assets/home/step-3.jpg" alt="spices"/>
+                    </div>
+                    <div class="step">
+                        <h2>Lastly</h2>
+                        <p>Nullam feugiat nibh vitae ullamcorper pharetra. Nunc eleifend elit sed quam mollis tempor. Nam sit amet dignissim urna. Suspendisse eget elit luctus, semper est sit amet, faucibus nunc. Nunc convallis gravida risus, eget fringilla tortor cursus a. In sit amet leo viverra, efficitur felis in, consectetur magna. Maecenas placerat non augue ut sodales. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+                    </div>
+                </div>
+            </div>
         </div>
     </body>
 </html>

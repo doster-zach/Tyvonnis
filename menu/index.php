b2c52bb1fea3310ce86489d8bd41d647ac7384b3
Zachary Doster <zacharydoster@Zacharys-MacBook-Pro.local>
2019-04-27 20:52:18 -0500

    Menu items rough first pass

--- menu/index.php
+++ menu/index.php
@@ -16,6 +16,22 @@
                 </div>
             </div>
         </div>
+        <div class="container">
+          <div class="menu-grid">
+          <?php 
+              $menu_file_names = array("fruit-spread.jpg", "burrito-bar.jpg", "burger.jpg", "salad.jpg", "chicken.jpg", "breakfast.jpg");
+              $menu_display_names = array("Fruit Spreads", "Burrito Bar", "Premium Burgers", "Salads", "Chicken", "Breakfast");
+              $menu_links = array("#","#","#","#","#","#");
+
+              for($i = 0; $i < sizeof($menu_display_names); $i++) {
+                  $file_name = $menu_file_names[$i];
+                  $display_name = $menu_display_names[$i];
+                  $item_link = $menu_links[$i];
+                  require '../partials/menu-item.php';
+              }
+          ?>
+          </div>
+        </div>
         <?php require '../partials/footer.php'; ?>
     </body>
 </html>
---
+++ partials/menu-item.php
@@ -0,0 +1,9 @@
+<a class="menu-link" href="<?= $item_link ?>">
+  <div class="menu-item medium-up--one-third" style="background-image: url('../assets/menu/<?= $file_name ?>'">
+      <div class="img-overlay">
+      </div>
+      <div class="breakdown-text">
+          <h2><?= $display_name ?></h2>    
+      </div>
+  </div>
+</a>

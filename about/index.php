51923b4a70cc350d0fcb3815339e964948adaf26
Zachary Doster <zacharydoster@Zacharys-MacBook-Pro.local>
2019-04-28 15:45:17 -0500

    About page

--- about/index.php
+++ about/index.php
@@ -16,6 +16,30 @@
                 </div>
             </div>
         </div>
+        <div class="container">
+            <div class="about-quote">
+                <p>
+                Cooking is all about people.
+                Food is maybe the only universal thing that really has the power to bring everyone together.
+                No matter what culture,
+                everywhere around the world, people get together to eat. 
+                </p>
+            </div>
+            <div class="about-item-wrapper">
+            <?php 
+                $about_file_names = array("fruit-spread.jpg", "burrito-bar.jpg", "burger.jpg", "salad.jpg");
+                $about_headers = array("Fruit Spreads", "Burrito Bar", "Premium Burgers", "Salads");
+                $about_content = array("Donec rutrum eu leo in ullamcorper. Sed laoreet maximus ullamcorper. Ut efficitur, arcu id accumsan lobortis, dolor sapien suscipit quam, vitae eleifend mauris ex ac lorem. Curabitur feugiat orci ut venenatis rhoncus. Nam sodales placerat pellentesque. Mauris sodales viverra metus vel interdum. Praesent a ipsum mollis, tincidunt orci at, gravida est. Aliquam finibus tellus eget felis posuere ornare. Duis vestibulum justo id tellus dictum auctor. Ut porta pharetra dui, eu iaculis justo pretium a.","Donec rutrum eu leo in ullamcorper. Sed laoreet maximus ullamcorper. Ut efficitur, arcu id accumsan lobortis, dolor sapien suscipit quam, vitae eleifend mauris ex ac lorem. Curabitur feugiat orci ut venenatis rhoncus. Nam sodales placerat pellentesque. Mauris sodales viverra metus vel interdum. Praesent a ipsum mollis, tincidunt orci at, gravida est. Aliquam finibus tellus eget felis posuere ornare. Duis vestibulum justo id tellus dictum auctor. Ut porta pharetra dui, eu iaculis justo pretium a.","Donec rutrum eu leo in ullamcorper. Sed laoreet maximus ullamcorper. Ut efficitur, arcu id accumsan lobortis, dolor sapien suscipit quam, vitae eleifend mauris ex ac lorem. Curabitur feugiat orci ut venenatis rhoncus. Nam sodales placerat pellentesque. Mauris sodales viverra metus vel interdum. Praesent a ipsum mollis, tincidunt orci at, gravida est. Aliquam finibus tellus eget felis posuere ornare. Duis vestibulum justo id tellus dictum auctor. Ut porta pharetra dui, eu iaculis justo pretium a.","Donec rutrum eu leo in ullamcorper. Sed laoreet maximus ullamcorper. Ut efficitur, arcu id accumsan lobortis, dolor sapien suscipit quam, vitae eleifend mauris ex ac lorem. Curabitur feugiat orci ut venenatis rhoncus. Nam sodales placerat pellentesque. Mauris sodales viverra metus vel interdum. Praesent a ipsum mollis, tincidunt orci at, gravida est. Aliquam finibus tellus eget felis posuere ornare. Duis vestibulum justo id tellus dictum auctor. Ut porta pharetra dui, eu iaculis justo pretium a.");
+
+                for($i = 0; $i < sizeof($about_file_names); $i++) {
+                    $file_name = $about_file_names[$i];
+                    $header = $about_headers[$i];
+                    $content = $about_content[$i];
+                    require '../partials/about-item.php';
+                }
+            ?>
+            </div>
+        </div>
         <?php require '../partials/footer.php'; ?>
     </body>
 </html>
---
+++ partials/about-item.php
@@ -0,0 +1,5 @@
+<div class="medium-up--one-half about-item <?= $i % 2 == 0 ? 'left' : 'right' ?>">
+  <div class="img-container" style="background-image: url('../assets/menu/<?= $file_name ?>')"></div>
+  <h2><?= $header ?></h2>
+  <p><?= $content ?></p>
+</div>

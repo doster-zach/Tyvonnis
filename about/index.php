28717587d21736782df5b327a991c7d932b03546
Zachary Doster <zacharydoster@Zacharys-MacBook-Pro.local>
2019-04-27 20:19:58 -0500

    Added héros to about and menu pages and updated home page image

--- about/index.php
+++ about/index.php
@@ -6,7 +6,16 @@
     </head>
     <body>
         <?php require '../partials/nav.php'; ?>
-        
+        <div class="container-full">
+            <div class="about hero-container">
+                <div class="img-overlay">
+                </div>
+                <div class="hero-text-content">
+                    <h1>About Tyvonni's</h1>
+                    <h2>How we came to be</h2>    
+                </div>
+            </div>
+        </div>
         <?php require '../partials/footer.php'; ?>
     </body>
 </html>
--- menu/index.php
+++ menu/index.php
@@ -6,7 +6,16 @@
     </head>
     <body>
         <?php require '../partials/nav.php'; ?>
-        
+        <div class="container-full">
+            <div class="menu hero-container">
+                <div class="img-overlay">
+                </div>
+                <div class="hero-text-content">
+                    <h1>Our Menu</h1>
+                    <h2>Build Your Catering Event</h2>    
+                </div>
+            </div>
+        </div>
         <?php require '../partials/footer.php'; ?>
     </body>
 </html>

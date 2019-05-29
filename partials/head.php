6b7e48c99757d845648797a8f5d036eb06e1c900
Zachary Doster <zacharydoster@Zacharys-MacBook-Pro.local>
2019-05-29 18:31:35 -0500

    add this to make the mobile breakpoints work for actual mobile screens

--- partials/head.php
+++ partials/head.php
@@ -1,2 +1,3 @@
 <link rel='stylesheet' href='../css/theme.css'/>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+<meta name="viewport" content="width=device-width, initial-scale=1">
